6ad880623f299b51f4581792328f3a1d985a6911
saketh612 <saketh.repaka01@gmail.com>
2023-06-02 15:24:47 +0530

    made some changes to the news-app

---
+++ src/components/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is a news component
+        <Newsitem/>
+      </div>
+    )
+  }
+}
+
+export default News
